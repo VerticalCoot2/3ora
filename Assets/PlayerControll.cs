bfc830671475c8db133537d85b7d88c204e42abb
verticalcoot2 <verticalcoot2@gmail.com>
2026-03-03 18:30:07 +0100

    comit

--- Assets/PlayerControll.cs
+++ Assets/PlayerControll.cs
@@ -8,18 +8,30 @@ public class PlayerControll : MonoBehaviour
     [SerializeField] float speed = 10;
     [SerializeField] float turnSpeed = 140;
 
+    [SerializeField] bool grounded = false;
 
-    [SerializeField] private Material BodyColor;
-    [SerializeField] private Material white;
+
+    private Material BodyColor;
+    private Material white;
     bool blinking = false;
+
+    Rigidbody rb;
+    float jumpForce = 35;
 
     void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+
         BodyColor = transform.GetChild(0).GetComponent<Renderer>().material;
 
         white = transform.GetChild(0).
             transform.GetChild(0).
             transform.GetChild(0).GetComponent<Renderer>().material;
+    }
+
+    void Start()
+    {
+        StartCoroutine(BlinkAction());
     }
     
     [SerializeField] void Blink()
@@ -33,29 +45,35 @@ public class PlayerControll : MonoBehaviour
 
     IEnumerator BlinkAction()
     {
-        GameObject eye1 = transform.GetChild(0).
+        while(true)
+        {
+            GameObject eye1 = transform.GetChild(0).
             transform.GetChild(0).
             transform.GetChild(0).gameObject;
-        GameObject eye2 = transform.GetChild(0).
-            transform.GetChild(0).
-            transform.GetChild(1).gameObject;
 
-        GameObject innerEye1 = eye1.transform.GetChild(0).gameObject;
-        GameObject innerEye2 = eye2.transform.GetChild(0).gameObject;
+            GameObject eye2 = transform.GetChild(0).
+                transform.GetChild(0).
+                transform.GetChild(1).gameObject;
 
-        eye1.GetComponent<Renderer>().material = BodyColor;
-        eye2.GetComponent<Renderer>().material = BodyColor;
-        innerEye1.SetActive(false);
-        innerEye2.SetActive(false);
+            GameObject innerEye1 = eye1.transform.GetChild(0).gameObject;
+            GameObject innerEye2 = eye2.transform.GetChild(0).gameObject;
 
-        yield return new WaitForSeconds(0.1f);
+            eye1.GetComponent<Renderer>().material = BodyColor;
+            eye2.GetComponent<Renderer>().material = BodyColor;
 
-        eye1.GetComponent<Renderer>().material = white;
-        eye2.GetComponent<Renderer>().material = white;
-        innerEye1.SetActive(true);
-        innerEye2.SetActive(true);
+            innerEye1.SetActive(false);
+            innerEye2.SetActive(false);
 
-        blinking = false;
+            yield return new WaitForSeconds(0.1f);
+
+            eye1.GetComponent<Renderer>().material = white;
+            eye2.GetComponent<Renderer>().material = white;
+
+            innerEye1.SetActive(true);
+            innerEye2.SetActive(true);
+
+            yield return new WaitForSeconds(Random.Range(4f, 6f));
+        }
     }
 
     void Update()
@@ -79,11 +97,31 @@ public class PlayerControll : MonoBehaviour
             transform.RotateAround(transform.position, Vector3.up, -turnSpeed * Time.deltaTime);
         }
             #endregion
+
         if(Input.GetKey(KeyCode.Space))
         {
-            Blink();
+            if(grounded)
+            {
+                rb.AddForce(new Vector3(0, jumpForce*10, 0));
+                grounded = false;
+            }
         }
     }
 
-    
+    private void OnCollisionEnter(Collision col)
+    {
+        //if(col.gameObject.tag == "Ground")
+        //{
+        //    grounded = true;
+        //}
+
+        switch (col.gameObject.tag)
+        {
+            case "Ground":
+                grounded = true;
+                break;
+        }
+    }
+
+
 }
